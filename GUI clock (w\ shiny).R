3240567bdcac3136dea29ef64d4faa970609dce5
Iskander <gua4dian@gmail.com>
2021-11-09 18:20:56 +0300

    Update and rename clock.R to GUI clock (w\ shiny).R

--- GUI clock (w\ shiny).R
+++ GUI clock (w\ shiny).R
@@ -5,8 +5,7 @@ library(miniUI)
 
 clock <- function() {
   
-  # Our ui will be a simple gadget page, which
-  # simply displays the time in a 'UI' output.
+  # gadget page
   ui <- miniPage(
     gadgetTitleBar("Clock"),
     miniContentPanel(
@@ -16,7 +15,7 @@ clock <- function() {
   
   server <- function(input, output, session) {
     
-    # Set some CSS styles for our clock.
+    # Css confige
     clockStyles <- paste(
       "border: 1px solid #DADADA",
       "background-color: #EFEFEF",
@@ -27,24 +26,21 @@ clock <- function() {
       sep = "; "
     )
     
-    # We'll use a 'reactiveTimer()' to force Shiny
-    # to update and show the clock every second.
+    # reactiveTimer for real-time update
     invalidatePeriodically <- reactiveTimer(intervalMs = 1000)
     observe({
       
-      # Call our reactive timer in an 'observe' function
-      # to ensure it's repeatedly fired.
       invalidatePeriodically()
       
-      # Get the time, and render it as a large paragraph element.
+      # Get the time
+      # render it as a large paragraph element.
       time <- Sys.time()
       output$time <- renderUI({
         p(style = clockStyles, time)
       })
     })
     
-    # Listen for 'done' events. When we're finished, we'll
-    # insert the current time, and then stop the gadget.
+    # stop gadget
     observeEvent(input$done, {
       timeText <- paste0("\"", as.character(Sys.time()), "\"")
       rstudioapi::insertText(timeText)
@@ -53,8 +49,7 @@ clock <- function() {
     
   }
   
-  # We'll use a pane viwer, and set the minimum height at
-  # 300px to ensure we get enough screen space to display the clock.
+  # implementing pane viewer
   viewer <- paneViewer(300)
   runGadget(ui, server, viewer = viewer)
   
